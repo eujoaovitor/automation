c36eb0f4bbce1c0e83d60a5096844642e1ea4f44
eujoaovitor <joaovitorqxd@gmail.com>
2023-08-22 21:10:12 -0300

    funções iniciadas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const inicioFluxo = document.getElementById('inicio-fluxo');
-  const fluxoTitle = document.querySelector('.title h1');
-  const publishButton = document.querySelector('.publish-button');
-
-  let offsetX, offsetY, dragging = false;
-
 
   const zoomInButton = document.querySelector('.zoom-in');
   const zoomOutButton = document.querySelector('.zoom-out');
-  const flowCard = document.querySelector('.flow-card-start');
+  const groupCard = document.querySelector('.group-card');
+  const toggleNavbarButton = document.getElementById("toggleNavbarButton");
+  const navbar = document.querySelector(".navbar");
 
   let currentScale = 1; // Valor inicial da escala
   const maxZooms = 5; // Máximo de zooms permitidos
@@ -16,54 +12,64 @@ document.addEventListener('DOMContentLoaded', () => {
   zoomInButton.addEventListener('click', () => {
     if (currentScale < 1.5) { // Verifica se a escala não ultrapassa o limite
       currentScale += 0.1; // Incrementa o valor da escala
-      flowCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
+      groupCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
     }
   });
 
   zoomOutButton.addEventListener('click', () => {
     if (currentScale > 0.6) { // Verifica se a escala não ultrapassa o limite
       currentScale -= 0.1; // Decrementa o valor da escala
-      flowCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
+      groupCard.style.transform = `scale(${currentScale})`; // Aplica a nova escala
     }
   });
 
-  inicioFluxo.addEventListener('mousedown', e => {
-    if (e.button !== 0) return;
+  let navbarOpen = true;
 
-    if (!inicioFluxo.classList.contains('active')) {
-      inicioFluxo.classList.add('active');
-
+  toggleNavbarButton.addEventListener("click", () => {
+    navbarOpen = !navbarOpen;
+    if (navbarOpen) {
+      navbar.style.transform = "translateX(0)";
+      main.style.width = "70%";
+    } else {
+      navbar.style.transform = "translateX(-320px)";
     }
-
-    offsetX = e.clientX - inicioFluxo.getBoundingClientRect().left;
-    offsetY = e.clientY - inicioFluxo.getBoundingClientRect().top;
-    dragging = true;
-
-    e.preventDefault();
-  });
-
-  document.addEventListener('mousemove', e => {
-    if (!dragging) return;
-
-    inicioFluxo.style.left = e.clientX - offsetX + 'px';
-    inicioFluxo.style.top = e.clientY - offsetY + 'px';
-  });
-
-  document.addEventListener('mouseup', () => {
-    dragging = false;
-  });
-
-  document.addEventListener('click', event => {
-    if (!inicioFluxo.contains(event.target)) {
-      inicioFluxo.classList.remove('active');
-    }
-  });
-
-  publishButton.addEventListener('click', () => {
-    const title = fluxoTitle.textContent;
-    alert(`Publicando fluxo com título: ${title}`);
   });
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".group-card");
+  let activeCard = null;
+  let offsetX = 0;
+  let offsetY = 0;
 
+  cards.forEach(card => {
+      card.addEventListener("mousedown", startDragging);
+      card.addEventListener("mouseup", stopDragging);
+  });
+
+  function startDragging(event) {
+      activeCard = event.currentTarget;
+      activeCard.style.cursor = "grabbing";
+
+      offsetX = event.clientX - activeCard.getBoundingClientRect().left;
+      offsetY = event.clientY - activeCard.getBoundingClientRect().top;
+
+      document.addEventListener("mousemove", dragCard);
+  }
+
+  function dragCard(event) {
+      if (activeCard) {
+          const x = event.clientX - offsetX;
+          const y = event.clientY - offsetY;
+          activeCard.style.transform = `translate(${x}px, ${y}px)`;
+      }
+  }
+
+  function stopDragging() {
+      if (activeCard) {
+          activeCard.style.cursor = "grab";
+          document.removeEventListener("mousemove", dragCard);
+      }
+  }
+});
